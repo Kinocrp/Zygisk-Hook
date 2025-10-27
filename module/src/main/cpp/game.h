ee2109ffd7eba2e7e330efbd6300556a36460990
KinoCrp <kino.crp@gmail.com>
2025-10-27 08:41:09 +0800

    Initial commit

---
+++ module/src/main/cpp/game.h
@@ -0,0 +1,10 @@
+//
+// Created by Perfare on 2020/7/4.
+//
+
+#ifndef ZYGISK_HOOK_GAME_H
+#define ZYGISK_HOOK_GAME_H
+
+#define GamePackageName "com.game.packagename"
+
+#endif //ZYGISK_HOOK_GAME_H
---
+++ module/src/main/cpp/hack.h
@@ -0,0 +1,12 @@
+//
+// Created by Perfare on 2020/7/4.
+//
+
+#ifndef ZYGISK_HOOK_HACK_H
+#define ZYGISK_HOOK_HACK_H
+
+#include <stddef.h>
+
+void hack_prepare(const char *game_data_dir, void *data, size_t length);
+
+#endif //ZYGISK_HOOK_HACK_H
